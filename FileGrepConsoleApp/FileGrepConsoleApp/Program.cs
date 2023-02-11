e6f2d5e642f9fff0765e78a82ce0b0b1ea9a21fd
yo1030 <0vant1c@gmail.com>
2023-02-11 11:42:29 +0900

    サンプルディレクトリの取得

--- FileGrepConsoleApp/FileGrepConsoleApp/Program.cs
+++ FileGrepConsoleApp/FileGrepConsoleApp/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+
+string curDir = Environment.CurrentDirectory;
+string[] sampleDir = getSampleDirectory(curDir);
+for (int i = 0;i < sampleDir.Length; i++)
+{
+    Console.WriteLine(sampleDir[i]);
+}
+string[] getSampleDirectory(string path)
+{
+    string[] directories = Directory.GetDirectories(path, "*_samples");
+    while(directories.Length == 0)
+    {
+        DirectoryInfo? parentDirInfo = Directory.GetParent(path);
+        if (parentDirInfo == null)
+        {
+            directories[0] = "Error:can't find sample directory";
+        } else
+        {
+            path = parentDirInfo.FullName;
+            directories = Directory.GetDirectories(path, "*_samples");
+        }
+    }
+    return directories;
+}
