b151b925704699e84024eea5b522f91b9c5a1305
yo1030 <0vant1c@gmail.com>
2023-02-11 13:26:25 +0900

    関数の外だし

---
+++ FileGrepConsoleApp/FileGrepConsoleApp/Function.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileGrepConsoleApp
+{
+    internal class Function
+    {
+        internal string[] getSampleDirectory(string path)
+        {
+            string[] directories = Directory.GetDirectories(path, "*_samples");
+            while (directories.Length == 0)
+            {
+                DirectoryInfo? parentDirInfo = Directory.GetParent(path);
+                if (parentDirInfo == null)
+                {
+                    directories[0] = "Error:can't find sample directory";
+                }
+                else
+                {
+                    path = parentDirInfo.FullName;
+                    directories = Directory.GetDirectories(path, "*_samples");
+                }
+            }
+            return directories;
+        }
+    }
+}
