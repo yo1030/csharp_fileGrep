83d01f286f8cbbaf6cb2b6f6d1857d6cec029acd
yo1030 <0vant1c@gmail.com>
2023-02-11 19:24:50 +0900

    htmlcss, php, c#毎のフォルダとファイル検索、ファイル一覧をresultフォルダに作成

--- FileGrepConsoleApp/FileGrepConsoleApp/Function.cs
+++ FileGrepConsoleApp/FileGrepConsoleApp/Function.cs
@@ -10,7 +10,7 @@ namespace FileGrepConsoleApp
     {
         internal string[] getSampleDirectory(string path)
         {
-            string[] directories = Directory.GetDirectories(path, "*_samples");
+            string[] directories = Directory.GetDirectories(path.ToLower(), "*_sample*");
             while (directories.Length == 0)
             {
                 DirectoryInfo? parentDirInfo = Directory.GetParent(path);
@@ -21,10 +21,58 @@ namespace FileGrepConsoleApp
                 else
                 {
                     path = parentDirInfo.FullName;
-                    directories = Directory.GetDirectories(path, "*_samples");
+                    directories = Directory.GetDirectories(path.ToLower(), "*_sample*");
                 }
             }
             return directories;
+        }
+        internal void createResult(string[] targetDir)
+        {
+            bool isHTML = false;
+            bool isPHP = false;
+            bool isCSHARP = false;
+            for (int i = 0;i < targetDir.Length; i++)
+            {
+                string targetDirName = targetDir[i];
+                DirectoryInfo targetDirInfo = new DirectoryInfo(targetDirName);
+                string dirName = targetDirInfo.Name.ToLower();
+                isHTML = (dirName.IndexOf("htmlcss") != -1);
+                isPHP = (dirName.IndexOf("php") != -1);
+                isCSHARP = (dirName.IndexOf("csharp") != -1);
+                if (isHTML)
+                {
+                    outputFileList(targetDirName, "html");
+                    isHTML = false;
+                }
+                if (isPHP)
+                {
+                    outputFileList(targetDirName, "php");
+                    isPHP = false;
+                }
+                if (isCSHARP)
+                {
+                    outputFileList(targetDirName, "cs");
+                    isCSHARP = false;
+                }
+            }
+            void outputFileList(string targetDir, string type)
+            {
+                string suffixName = "result" + type.ToUpper() + ".txt";
+                DirectoryInfo dirInfo = new DirectoryInfo(targetDir);
+                DirectoryInfo? parDirInfo = Directory.GetParent(targetDir);
+                IEnumerable<string> fileList = dirInfo.EnumerateFiles("*." + type, SearchOption.AllDirectories)
+                                                        .Select(f => f.Name);
+                if (parDirInfo == null)
+                {
+                    string? rootDir = Path.GetPathRoot(dirInfo.FullName);
+                    File.WriteAllLines(rootDir + @"\" + suffixName, fileList, Encoding.UTF8);
+                }
+                else
+                {
+                    DirectoryInfo resultDir = Directory.CreateDirectory(parDirInfo.FullName + @"\result");
+                    File.WriteAllLines(resultDir + @"\" + suffixName, fileList, Encoding.UTF8);
+                }
+            }
         }
     }
 }
--- FileGrepConsoleApp/FileGrepConsoleApp/Program.cs
+++ FileGrepConsoleApp/FileGrepConsoleApp/Program.cs
@@ -1,26 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
+using System.Text;
+using FileGrepConsoleApp;
 
 string curDir = Environment.CurrentDirectory;
-string[] sampleDir = getSampleDirectory(curDir);
-for (int i = 0;i < sampleDir.Length; i++)
-{
-    Console.WriteLine(sampleDir[i]);
-}
-string[] getSampleDirectory(string path)
-{
-    string[] directories = Directory.GetDirectories(path, "*_samples");
-    while(directories.Length == 0)
-    {
-        DirectoryInfo? parentDirInfo = Directory.GetParent(path);
-        if (parentDirInfo == null)
-        {
-            directories[0] = "Error:can't find sample directory";
-        } else
-        {
-            path = parentDirInfo.FullName;
-            directories = Directory.GetDirectories(path, "*_samples");
-        }
-    }
-    return directories;
-}
+Function fc = new Function();
+
+string[] sampleDir = fc.getSampleDirectory(curDir);
+fc.createResult(sampleDir);
